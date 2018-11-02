2c2b3c3b8160d1d767baae414dab5000dc642bc1
GUOHAI <guohai@gyyx.cn>
2018-11-02 21:23:44 +0800

    Initial Commit

---
+++ hospital-registration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hospital-registration
+//
+//  Created by 郭海 on 2018/11/2.
+//  Copyright © 2018 gh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ hospital-registrationTests/hospital_registrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  hospital_registrationTests.swift
+//  hospital-registrationTests
+//
+//  Created by 郭海 on 2018/11/2.
+//  Copyright © 2018 gh. All rights reserved.
+//
+
+import XCTest
+@testable import hospital_registration
+
+class hospital_registrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
